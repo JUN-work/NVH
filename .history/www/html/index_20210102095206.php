3032c209a149228a0adbe209a8867cddbec53f5f
飯田　純 <iidajun@jun.local>
2021-01-02 10:41:49 +0900

    css変更

---
+++ .history/www/html/index_20210102095206.php
@@ -0,0 +1,26 @@
+<?php
+
+$title = '';
+include('./common_php/_header.php');
+
+?>
+
+  <section class="main-title">
+    <!-- <div class="container"> -->
+    <div>
+		<img src="img/NVH-TOP.jpg" alt="TOP-img">
+    <h1 class="title">
+      NVH
+	  </h1>
+	  <h2 class="title2">
+	  Nutritional value List Site of Vegetables to your Health
+	  </h2>
+      <p class="subtitle">
+	  	あなたの<strong>健康</strong>のための野菜栄養価一覧サイト
+      </p>
+    </div>
+  </section>
+
+<?php
+
+include('./common_php/_footer.php');
---
+++ .history/www/html/index_20210102095341.php
@@ -0,0 +1,24 @@
+<?php
+
+$title = '';
+include('./common_php/_header.php');
+
+?>
+
+  <section class="main-title">
+    <!-- <div class="container"> -->
+		<img src="img/NVH-TOP.jpg" alt="TOP-img">
+    <h1 class="title">
+      NVH
+	  </h1>
+	  <h2 class="title2">
+	  Nutritional value List Site of Vegetables to your Health
+	  </h2>
+      <p class="subtitle">
+	  	あなたの<strong>健康</strong>のための野菜栄養価一覧サイト
+      </p>
+  </section>
+
+<?php
+
+include('./common_php/_footer.php');
